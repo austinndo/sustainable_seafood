e10ea15619f2b54f5dddf95c94e66e162ee453fa
Austin Do <austinndo@gmail.com>
2022-07-05 08:17:33 -0700

    Successfully read the species names. Ablr to sort them in alphabetical order and store them in another array

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 //Sustainable Seafood
 
-const searchButton = document.querySelector(`button`)
+const searchButton = document.getElementById(`search`)
 
 searchButton.addEventListener('click', async () => {
-  let response = await axios.get(`https://www.fishwatch.gov/api/species`)
-  console.log(response.data)
+  let response = await axios.get(`https://www.fishwatch.gov/api/species/`)
+  let speciesArray = []
+  for (let i = 0; i < response.data.length; i++) {
+    const speciesName = response.data[i]["Species Name"]
+    speciesArray.push(speciesName)
+    speciesArray.sort()
+  }
+  console.log(speciesArray)
+  
 } 
 )
 
+//map
+//sort
+//filter
 
 
